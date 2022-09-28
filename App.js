2cca03d43fafd80e4150b1edfba8567b956524d3
Jake Park <glow153@gmail.com>
2022-09-28 15:10:36 +0900

    moved example to separated repo

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SongListScreen } from './src/pages';
+
+const App = () => {
+  return (
+    <>
+      <SongListScreen />
+    </>
+  );
+};
+
+export default App;
